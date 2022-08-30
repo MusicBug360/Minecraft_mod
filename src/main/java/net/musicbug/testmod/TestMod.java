1e4dcfd379a79190124feda7cfbdb9634f8e66bb
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 21:29:40 -0500

    removed important comment

--- src/main/java/net/musicbug/testmod/TestMod.java
+++ src/main/java/net/musicbug/testmod/TestMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//very important comment
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
